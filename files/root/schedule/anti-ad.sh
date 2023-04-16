31ab344a7c4f3cbb92998adf2582de8e73b139ed
captaincai <captain.cai@icloud.com>
2023-04-16 22:21:57 +0800

    remove ad dbl

--- files/root/schedule/anti-ad.sh
+++ files/root/schedule/anti-ad.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 wget -O /etc/mosdns/rule/adlist.txt https://anti-ad.net/domains.txt
-wget -O /etc/mosdns/rule/ad-dbl.txt https://raw.githubusercontent.com/ookangzheng/dbl-oisd-nl/master/dbl.txt
 wget -O /etc/mosdns/rule/ad-qiu-simon.txt https://raw.githubusercontent.com/QiuSimons/openwrt-mos/master/dat/serverlist.txt
 service mosdns restart
