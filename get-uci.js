80a1c1c55edae645efa59cdb0514a13caf8e7c2c
captaincai <captain.cai@icloud.com>
2023-06-20 21:42:21 +0800

    fix: 处理多行uci

--- get-uci.js
+++ get-uci.js
@@ -1,5 +1,19 @@
 const getUci = (uciString, service) => {
-    const uciContent = uciString.split('\n').map((str) => str.trim().startsWith(`${service}.`) ?  `uci set ${str.trim()}` : str.trim())
+    const uciContent = uciString.split('\n').map((str) => {
+        const content = str.trim()
+        if(content.startsWith(`${service}.`)) {
+            const isList = content.split('=').pop().split(' ').length > 1
+            if(isList) {
+                const [key, value] = content.split('=');
+                const values = value.split(' ')
+                return values.map((v) => `uci add_list ${key}=${v}`)
+            } else {
+                return `uci set ${content}`
+            }
+        } else {
+            return content
+        }
+    })
     uciContent.push(`uci commit ${service}`)
-    return uciContent.join('\n ')
+    return uciContent.flat().join('\n ')
 }
